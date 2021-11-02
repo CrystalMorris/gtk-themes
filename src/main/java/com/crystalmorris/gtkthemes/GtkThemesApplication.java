9a047b6be739be7e425988127b44d226dadd08f3
CrystalM <81319362+CrystalMorris@users.noreply.github.com>
2021-11-01 21:50:44 -0500

    first commit

---
+++ src/main/java/com/crystalmorris/gtkthemes/GtkThemesApplication.java
@@ -0,0 +1,13 @@
+package com.crystalmorris.gtkthemes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtkThemesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtkThemesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crystalmorris/gtkthemes/GtkThemesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crystalmorris.gtkthemes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtkThemesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
